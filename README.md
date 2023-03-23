2ba0d79fa9789b73f1e3b42a962c48a85007c53c
Thales Soares <thales.soares@sensedia.com>
2023-03-23 19:59:43 -0300

    Creating mockData, scenes directory and configurations to theme.js with @mui/material

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Some dependencies to run this app:
 
 npm install @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo
+
+Some extensions used:
+
+- Tailwind Shades
--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-function App() {
-  return <div className="app">
-    <p>Olá Mundo</p>
-  </div>;
-}
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-export default App;
+export default function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app"></div>;
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
